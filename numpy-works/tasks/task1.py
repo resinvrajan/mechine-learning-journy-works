047a2d176afe9fad1a2ccd41e8ca8a44022ae9a5
RESIN V RAJAN <resinvrajan@gmail.com>
2026-01-07 20:25:04 +0530

    task1 added

---
+++ numpy-works/tasks/task1.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr=np.array([
+    [43,42,45,34,23],
+    [23,45,45,34,37],
+    [37,38,39,40,45]
+])
+print(arr[1,])
+print(arr[1,0])
+print(arr[:,2])
+print(arr[:,2:4])
+print(arr[1:3,1:3])
